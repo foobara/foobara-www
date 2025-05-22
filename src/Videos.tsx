7785dcf719501ca3ce91bbb2938e6bbb54d9ca3e
Miles Georgi <azimux@gmail.com>
2025-05-21 22:35:42 -0700

    Add some more videos

--- src/Videos.tsx
+++ src/Videos.tsx
@@ -5,6 +5,24 @@ function Videos () {
   return (
     <div className="Videos">
       <div>
+        <h1>Foobara::LLmBackedCommand: Write commands without logic and use LLM knowledge or decision-making
+          programmatically!</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/MsCCcz63umc?si=Lr-LMDx2nhAoBfmj"
+                title="YouTube video player" frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen></iframe>
+      </div>
+      <div>
+        <h1>Foobara MCP connector: Create an MCP server for your Ruby code with great ease!</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/_w3ZHdiJEGU?si=8N-7EkqIxp6RUjGQ"
+                title="YouTube video player" frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen>
+        </iframe>
+      </div>
+      <div>
         <h1>What is Foobara and why would one want to use a framework like this?</h1>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/SSOmQqjNSVY?si=yivq1SgJldU2ugDe"
                 title="YouTube video player" frameBorder="0"
