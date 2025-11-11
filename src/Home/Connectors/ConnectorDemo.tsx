6df255f119014d37ca46a31c9def4b6b8b6eea11
Miles Georgi <azimux@gmail.com>
2025-11-11 12:47:58 -0800

    Begin refactor of a connector demo tab into a generic ConnectorDemo component
    Co-authored-by: hamdi4-beep <hamdiakippuden@gmail.com>

---
+++ src/Home/Connectors/ConnectorDemo.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { FaGithub } from 'react-icons/fa'
+
+import '../../../Home.css'
+
+export interface TabInfo {
+  tabLabel: string
+  tabId: string
+  html: string
+  integrationName: string
+  integrationId: string
+  githubLink: string | undefined
+  videoLink: string | undefined
+}
+
+export function CliConnectorDemo (tabs: TabInfo[]) {
+  const [activeTabId, setActiveTabId] = useState('connector')
+
+  const activeTab: TabInfo = tabs.find((tab) => {
+    return tab.tabId === activeTabId
+  }) as unknown as TabInfo
+
+  const githubLink = activeTab.githubLink
+
+  const hasGithubLink: boolean = typeof githubLink === 'string'
+
+  const classes: string = `meme-text-meme ${activeTab.integrationId}`
+
+  return (<div className="example-with-image">
+    <div className="example-container wide">
+      <div className="tabs">
+        <div>
+          {tabs.map((tab) => {
+            return (<button
+              key={tab.tabId}
+              className={`tab ${activeTabId === tab.tabId ? 'active' : ''}`}
+              onClick={() => { setActiveTabId(tab.tabId) } }
+            >
+              {tab.tabLabel}
+            </button>)
+          })}
+        </div>
+      </div>
+
+      <div className="tab-content" style={{ position: 'relative' }}>
+        {hasGithubLink && (
+            <a
+              href={githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="tab-link"
+              style={{ position: 'absolute', top: '3.5rem', right: '0.75rem', zIndex: 10 }}
+            >
+              View on GitHub <span style={{ marginLeft: '0.4rem', display: 'inline-block', verticalAlign: 'middle', position: 'relative', top: '1px' }}>{FaGithub({ size: 16 })}</span>
+            </a>
+        )
+        }
+        <pre><code dangerouslySetInnerHTML={{ __html: activeTab.html }} /></pre>
+      </div>
+    </div>
+
+    <div className="capy-and-dog-image-container">
+      <img
+        src="/images/capy_and_dog.jpg"
+        alt="Capybara and dog"
+        className="capy-and-dog-image"
+      />
+      <div className="meme-text meme-text-foobara">Foobara</div>
+      <div className={classes}>{activeTab.integrationName}</div>
+      <p className="image-attribution">
+        <a
+          href="https://linktr.ee/darkwingswildlife"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dark Wings Wildlife & Education
+        </a>
+      </p>
+    </div>
+  </div>)
+}
