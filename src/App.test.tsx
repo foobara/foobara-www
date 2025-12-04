c18e08e53e408e9bba946754d7e4eb423b77872f
Miles Georgi <azimux@gmail.com>
2025-12-03 20:21:58 -0800

    Fix test suite

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 
 test('renders learn react link', () => {
-  render(<BrowserRouter><App /></BrowserRouter>)
+  render(
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  )
   const linkElement = screen.getByText(/Home/i)
   expect(linkElement).toBeInTheDocument()
 })
